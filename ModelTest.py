154b3c2dec31fc207072711c9aeb291c91e7e99f
Enthusiast101 <89479662+Enthusiast101@users.noreply.github.com>
2023-07-18 15:37:51 +0530

    Improved Model

--- ModelTest.py
+++ ModelTest.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 mnist = tf.keras.datasets.mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-model = tf.keras.models.load_model("digit_classification_model.h5")
+model = tf.keras.models.load_model("digit_classification_model.h5", compile=False)
 
 y_pred = [np.argmax(val) for val in model.predict(X_test)]
 
@@ -15,7 +16,7 @@ print("Classification Report: \n", classification_report(y_test, y_pred))
 
 
 cm = tf.math.confusion_matrix(labels=y_test,predictions=y_pred)
-import seaborn as sns
+
 plt.figure(figsize=(10, 7))
 sns.heatmap(cm, annot=True, fmt="d")
 plt.xlabel("Predicted")
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,63 +1,51 @@
+import json
 import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-from keras.models import Sequential
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import keras_tuner as kt
 
-mnist = tf.keras.datasets.mnist
+# Load the MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Size
-print(len(X_train))
-print(len(X_test))
+# Preprocess the data
+X_train = X_train.reshape(-1, 28, 28, 1) / 255.0
+X_test = X_test.reshape(-1, 28, 28, 1) / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
 
-# Layers / Neurons
-print(tf.test.is_gpu_available(cuda_only=True))
 
-X_train = X_train / 255
-X_test = X_test / 255
-print(tf.__version__)
+# Define the model-building function for Keras Tuner
+def build_model(data):
+    model = Sequential()
+    for _ in range(data["conv_layers"]):
+        model.add(Conv2D(filters=data["conv1_filters"],
+                         kernel_size=data["conv1_kernel"],
+                         activation='relu',
+                         padding='same',
+                         input_shape=(28, 28, 1)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# CNN Model
-def train_model():
-    data_augmentation = keras.Sequential([
-        layers.RandomRotation(0.1, input_shape=(28, 28, 1)),
-        layers.RandomZoom(0.1),
-    ])
+    model.add(Flatten())
+    for _ in range(data["deep_layers"]):
+        model.add(Dropout(rate=data['dropout']))
+        model.add(Dense(units=data['dense_units'], activation='relu'))
 
-    model = keras.Sequential([
-        data_augmentation,
-        # CNN
-        layers.Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation="relu"),
-        layers.MaxPool2D((2, 2)),
-        layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation="relu"),
-        layers.MaxPool2D((2, 2)),
-        layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation="relu"),
-        layers.MaxPool2D((2, 2)),
-        layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation="relu"),
-        layers.MaxPool2D((2, 2)),
-        # Network
-        layers.Flatten(),
-        layers.Dense(256, activation="relu"),
-        layers.Dropout(.2),
-        layers.Dense(128, activation="relu"),
-        layers.Dense(64, activation="relu"),
-        layers.Dropout(.2),
-        layers.Dense(32, activation="relu"),
-        layers.Dense(16, activation="relu"),
-        layers.Dense(10, activation="softmax")
-    ])
+    model.add(Dense(10, activation='softmax'))
 
-    model.compile(optimizer="RMSprop",
-                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=["accuracy"])
+    model.compile(optimizer=Adam(learning_rate=data['learning_rate']),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
-    model.fit(X_train, y_train, epochs=25, batch_size=32)
+    return model
 
-    model.save("digit_classification_model.h5")
 
-# Testing if CUDA is availaible
-if tf.test.is_gpu_available(cuda_only=True):
-    with tf.device("/GPU:0"):
-        train_model()
-else:
-    train_model()
+with open(r"tmp/tuned_vals.json", "r") as file:
+    data = json.load(file)
+
+model = build_model(data)
+model.fit(X_train, y_train, epochs=5)
+model.save("digit_classification_model.h5")
+
